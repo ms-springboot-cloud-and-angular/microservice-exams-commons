f1b7f22afa99c0e8723aacf4ac1a7dd63ad69be1
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-12 11:45:24 +0200

    New SpringBoot project - Common Exam

---
+++ src/main/java/com/joseluisestevez/ms/commons/exams/MicroserviceExamsCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.commons.exams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceExamsCommonsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceExamsCommonsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/joseluisestevez/ms/commons/exams/MicroserviceExamsCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joseluisestevez.ms.commons.exams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceExamsCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
