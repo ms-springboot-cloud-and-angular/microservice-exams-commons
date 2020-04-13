095c590625d27d7b61f46a94d25aec0a007ecf2b
Jose Luis Estevez Prieto <jose.estevez.prieto@gmail.com>
2020-04-13 17:38:18 +0200

    Add answered attribute

--- src/main/java/com/joseluisestevez/ms/commons/exams/models/entity/Exam.java
+++ src/main/java/com/joseluisestevez/ms/commons/exams/models/entity/Exam.java
@@ -17,6 +17,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -56,6 +57,9 @@ public class Exam {
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
     private Subject subject;
+
+    @Transient
+    private boolean answered;
 
     @PrePersist
     public void prePersist() {
